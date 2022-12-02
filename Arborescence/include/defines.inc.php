c79482330e40174fc922e33f1eb2a65b3920fa24
Raphaël D <daniel.rapahel@hotmail.fr>
2022-12-02 11:55:15 +0100

    f

--- Arborescence/include/defines.inc.php
+++ Arborescence/include/defines.inc.php
@@ -2,32 +2,20 @@
 
 
 // define database tables names
-define('DB_TABLE_PERSONNE', 'personne');
-define('DB_TABLE_COURS', 'cours');
-define('DB_TABLE_CHEVAL', 'cheval');
-define('DB_TABLE_ROBE', 'robe');
-define('DB_TABLE_TYPE_PENSION', 'type_pension');
-define('DB_TABLE_PENSION', 'pension');
+define('DB_TABLE_CLIENT', 'client');
+define('DB_TABLE_CONNEXION', 'connexion');
 
 //define paths
 define('DB_CLASS_DIR', 'class/');
 
 
 // get main classes
-include_once DB_CLASS_DIR.'Cavalier.class.inc.php';
-include_once DB_CLASS_DIR.'Cheval.class.inc.php';
-include_once DB_CLASS_DIR.'Robe.class.inc';
-include_once DB_CLASS_DIR.'Type_pension.class.inc';
-include_once DB_CLASS_DIR.'Pension.class.inc';
-include_once DB_CLASS_DIR.'Cours.class.inc.php';
+include_once DB_CLASS_DIR.'Client.class.inc.php';
+include_once DB_CLASS_DIR.'Connexion.class.inc.php';
 
 // get main objects
-$oCavalier = new Cavalier();
-$oCheval = new Cheval();
-$oRobe = new Robe();
-$oType_pension = new Type_pension();
-$oPension = new Pension();
-$oCours = new Cours();
+$oCavalier = new client();
+$oCheval = new connexion();
 try {
     $server = "localhost";
     $username = "root";

4dc5c9811163e162bd579fa913032d76c93c4bed
Raphaël D <daniel.rapahel@hotmail.fr>
2022-12-02 12:49:13 +0100

    Changement complet

---
+++ Arborescence/Dashboard/Dashboard.php
@@ -0,0 +1,36 @@
+<?php
+include('../include/defines.inc.php');
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../static/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "input" ).checkboxradio();
+  } );
+  </script>
+    <title>Personne</title>
+</head>
+<body>
+    <script>
+        //const url = "Cavalier_trait.php";
+    </script>
+    <?php
+        if(!isset($_GET["nav"]) || $_GET["nav"] === "read"){
+            
+        }elseif($_GET["nav"] === "inscription"){
+            
+        }elseif($_GET["nav"] === "connexion"){
+            
+        }elseif($_GET["nav"] === "")
+    ?>
+</body>
